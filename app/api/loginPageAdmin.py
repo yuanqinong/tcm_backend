5fb462cd5453de6e6ea1cc257c4217858ed72f7f
yuanqinong <yuanqin1108@gmail.com>
2024-11-07 12:23:17 +0800

    Remove full connection string for better security

--- app/api/loginPageAdmin.py
+++ app/api/loginPageAdmin.py
@@ -20,7 +20,12 @@ security = HTTPBearer()
 load_dotenv()
 
 # Database setup
-SQLALCHEMY_DATABASE_URL = os.getenv("USERDB_CONNECTION")
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_HOST = os.getenv("POSTGRES_HOST")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT")
+POSTGRES_DB = os.getenv("POSTGRES_DB")
+SQLALCHEMY_DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
--- tools/recommendation.py
+++ tools/recommendation.py
@@ -15,8 +15,12 @@ import json
 from pydantic import ValidationError
 
 load_dotenv()
-
-DATABASE_URL = os.getenv("USERDB_CONNECTION")
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_HOST = os.getenv("POSTGRES_HOST")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT")
+POSTGRES_DB = os.getenv("POSTGRES_DB")
+DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
 
 # Create the engine
 engine = sa.create_engine(DATABASE_URL)
