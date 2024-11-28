685781d73af4df97649bd82a8079c596eafacd90
yuanqinong <yuanqin1108@gmail.com>
2024-11-28 14:04:22 +0800

    Improved vision promt

--- app/utils/prompt/VisionPrompt.py
+++ app/utils/prompt/VisionPrompt.py
@@ -2,4 +2,5 @@ prompt = """
 You are a vision assistant. Your task is to analyze the image and provide a description of the image.
 If the image is a table, provide a markdown table with the data, and a short description of the table.
 If the image is a chart, provide a description of the chart and the data it contains. Make sure the labels or axis are correct with the data.
+If you are not sure about the type of the image, provide a general description of the image.
 """
